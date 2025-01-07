d64b735dec16c0b8c524776c9b5ac2d29d21c3c7
admin_dho <davidhorph@hotmail.com>
2025-01-07 00:50:51 -0600

    Added def main():

--- main.py
+++ main.py
@@ -4,39 +4,42 @@ if __name__ == "__main__":
    from termcolor import colored;
    from colorama import init;
    
-   init();
+   def main():
+      init();
 
-   GreetingInterface();
+      GreetingInterface();
 
-   input("Press any key to continue with your order...")
+      input("Press any key to continue with your order...")
 
-   ItemNumber, Quantity, _orderID, finished = 0, 0, 1, False;
+      ItemNumber, Quantity, _orderID, finished = 0, 0, 1, False;
 
-   try:
-      Budget:float = float(57.00);
+      try:
+         Budget:float = float(57.00);
 
-      while (finished == False) and (Budget > 1.99):
-         print("Your current Budget is now:", colored(Budget, "light_green", attrs=["bold"]));
+         while (finished == False) and (Budget > 1.99):
+            print("Your current Budget is now:", colored(Budget, "light_green", attrs=["bold"]));
 
-         ItemNumber, Quantity = map(lambda x: int(x), input("Enter the food item number (on the left) and the quantity you want to order => ").split());
+            ItemNumber, Quantity = map(lambda x: int(x), input("Enter the food item number (on the left) and the quantity you want to order => ").split());
 
-         print(f"\n{'='*51}\n");
+            print(f"\n{'='*51}\n");
 
-         OrderResult = App(ItemNumber, Quantity, Budget, _orderID);
+            OrderResult = App(ItemNumber, Quantity, Budget, _orderID);
 
-         if isinstance(OrderResult, dict): 
-            Budget = OrderResult.get("Budget");
-            _orderID+=1;
-         
-         print(OrderResult, end=f"\n\n{'*'*51}\n\n");
+            if isinstance(OrderResult, dict): 
+               Budget = OrderResult.get("Budget");
+               _orderID+=1;
+            
+            print(OrderResult, end=f"\n\n{'*'*51}\n\n");
 
-         isDone = input("Are you finished??? ");
-         print();
-         finished = False if isDone in ["no", "n"] else True;
-   
-   except ValueError as err:
-      print(f"Sorry... you MUST enter integers for itemNumber (you have {colored(ItemNumber, color='red', attrs=['bold'])}) and quantity (you have {colored(Quantity, color='red', attrs=['bold'])})!!! => {err}");
-   except IndexError as err:
-      print(f"It appears you selected an item number that is NOT on the menu. We only have 12 items and you selected {colored(ItemNumber, color='red', attrs=['bold'])} as an ItemNumber: {err}.");
+            isDone = input("Are you finished??? ");
+            print();
+            finished = False if isDone in ["no", "n"] else True;
+      
+      except ValueError as err:
+         print(f"Sorry... you MUST enter integers for itemNumber (you have {colored(ItemNumber, color='red', attrs=['bold'])}) and quantity (you have {colored(Quantity, color='red', attrs=['bold'])})!!! => {err}");
+      except IndexError as err:
+         print(f"It appears you selected an item number that is NOT on the menu. We only have 12 items and you selected {colored(ItemNumber, color='red', attrs=['bold'])} as an ItemNumber: {err}.");
+
+   main()
 
    
