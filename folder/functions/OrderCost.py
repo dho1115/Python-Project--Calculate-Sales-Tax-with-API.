0f3d7e2edd770a80de6b2e07cb890c7e7b2498ab
admin_dho <davidhorph@hotmail.com>
2025-01-07 15:37:02 -0600

    Added the Tip (0.2).

--- folder/functions/OrderCost.py
+++ folder/functions/OrderCost.py
@@ -1,10 +1,12 @@
 from folder.menu import Menu;
 from folder.functions.apis.state_tax_rate import SalesTax;
+from folder.restaurant_details import YummyTummyDetails;
 
 def SubTotal(quantity, itemNumber, Menu:list=Menu):
    item = list(filter(lambda x: x[0] == int(itemNumber), Menu))[0];
    CostPerItem = list(item[1].items())[0][1];
    subtotal = float(quantity)*CostPerItem;
-   TotalCost = round(subtotal + (subtotal*SalesTax()), 2);
+   Tip = round(subtotal*0.2, 2); #Added the tip.
+   TotalCost = round((subtotal + (subtotal*SalesTax(YummyTummyDetails.get("zip")))) + Tip, 2) #With Tip
 
-   return dict(message='Order placed SUCCESSFULLY!!!', orderplaced= dict(quantity=quantity, item=item[1]), SubTotal=float(subtotal), total=float(TotalCost))
+   return dict(message='Order placed SUCCESSFULLY!!!', orderplaced= dict(quantity=quantity, item=item[1]), SubTotal=float(subtotal), total=float(TotalCost)) #Logic for each order.
