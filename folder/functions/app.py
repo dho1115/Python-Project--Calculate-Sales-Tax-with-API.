cdad949fde8a52d42370853c3715b2682999933d
admin_dho <davidhorph@hotmail.com>
2025-01-07 00:52:44 -0600

    code cleanup.

--- folder/functions/app.py
+++ folder/functions/app.py
@@ -14,11 +14,16 @@ def App(ItemNumber:int, Quantity:int, Budget:float, _orderID:int):
             Order:dict = SubTotal(Quantity, ItemNumber, Menu);
 
             if Order.get('total') > Budget: 
-               raise ValueError(colored(f"Sorry, but the total of this order, ${Order.get('total')} CANNOT exceed your remaining budget of ${Budget}.", "red", attrs=['bold']));            
+               raise ValueError(colored(f"Sorry, but the total of this order, ${Order.get('total')} CANNOT exceed your remaining budget of ${Budget}.", "red", attrs=['bold']));
+                  
             CurrentOrders = [*CurrentOrders, dict(_id=_orderID, order={**Order.get("orderplaced") , "total": Order.get("total")})];
+
             CartTotal["grand total"] = CartTotal.get("grand total") + Order.get("total");
+
             print(colored(f"Grand Total so far is: {CartTotal['grand total']}", on_color="on_yellow", attrs=["bold"]));
+
             CartTotal = {**CartTotal, "all orders": CurrentOrders};
+            
             Budget-= Order.get("total");
 
             return dict(Budget=Budget, Order=Order, CartTotal=CartTotal);
